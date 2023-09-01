31c15204545b1df36bce2204647e5d05bcdea1b8
tutich <pkoinange02@gmail.com>
2023-09-02 01:19:51 +0300

    js style

--- cou.js
+++ cou.js
@@ -2,8 +2,8 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-btns.forEach(function (btn) {
-    btn.addEventListener("click", function (e) {
+btns.forEach(function (list) {
+    list.addEventListener("click", function (e) {
     const style = e.currentTarget.classList;
 if(style.contains("increase")) {
     count ++;
@@ -13,6 +13,15 @@ else if(style.contains("decrease")) {
 }
 else{
     count = 0;
+}
+if(count > 0) {
+    value.style.color = "green";
+}
+if(count < 0) {
+    value.style.color = "red";
+}
+if(count === 0) {
+    value.style.color = "black";
 }
 value.textContent = count;
     });
