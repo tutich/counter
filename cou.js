30d7048b93d782dc157b33dc70bb46091c62b113
tutich <pkoinange02@gmail.com>
2023-09-04 13:29:18 +0300

    js html comment

--- cou.js
+++ cou.js
@@ -1,28 +1,28 @@
-let count = 0;
-const value = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn");
+// let count = 0;
+// const value = document.querySelector("#value");
+// const btns = document.querySelectorAll(".btn");
 
-btns.forEach(function (list) {
-    list.addEventListener("click", function (e) {
-    const style = e.currentTarget.classList;
-if(style.contains("increase")) {
-    count ++;
-}
-else if(style.contains("decrease")) {
-    count --;
-}
-else{
-    count = 0;
-}
-if(count > 0) {
-    value.style.color = "green";
-}
-if(count < 0) {
-    value.style.color = "red";
-}
-if(count === 0) {
-    value.style.color = "black";
-}
-value.textContent = count;
-    });
-});
+// btns.forEach(function (list) {
+//     list.addEventListener("click", function (e) {
+//     const style = e.currentTarget.classList;
+// if(style.contains("increase")) {
+//     count ++;
+// }
+// else if(style.contains("decrease")) {
+//     count --;
+// }
+// else{
+//     count = 0;
+// }
+// if(count > 0) {
+//     value.style.color = "green";
+// }
+// if(count < 0) {
+//     value.style.color = "red";
+// }
+// if(count === 0) {
+//     value.style.color = "black";
+// }
+// value.textContent = count;
+//     });
+// });
