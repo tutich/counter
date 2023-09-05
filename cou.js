565584f398437e7a41d1ce7970fc2550a38b25f8
tutich <pkoinange02@gmail.com>
2023-09-05 21:38:32 +0300

    js

--- cou.js
+++ cou.js
@@ -89,3 +89,13 @@
 //     showPerson();
 //  })
 
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function() {
+    if(links.classList.contains("show-links")) {
+links.classList.remove("show-lnks");
+    } else {
+        links.classList.add("show-links");
+    }
+});
