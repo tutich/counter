09e45fc0bd3fbd5f3cab8b25af3bf361a34f0ca6
tutich <pkoinange02@gmail.com>
2023-09-05 19:00:00 +0300

    html structure

--- cou.js
+++ cou.js
@@ -27,62 +27,65 @@
 //     });
 // });
 
-const reviews = [
-    {
-        Id: 1,
-        name: "Rose chep",
-        job: "Web Developer",
-        img: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-964894534-1600088334.jpg",
-        text: "Would recommend anyone to get their glasses here.Top quality!",
-    },
-    {
-        id: 2,
-        name: "josh Werner",
-        job:  "Ux Designer",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeIOfjYdgqb2559aXQSwMOfEfQoCePwANbKw&usqp=CAU",
-        text: "Since i got this glasses my coding life has improved significantly due to reduced stress on my eyes",
-    }
-];
-const img = document.getElementById("img");
- const author = document.getElementById("author");
- const job = document.getElementById("job");
- const desc = document.getElementById("description");
 
- const prev = document.querySelector(".prev");
- const next = document.querySelector(".next");
- const random = document.querySelector(".random");
 
- let currentItem = 0;
+// const reviews = [
+//     {
+//         Id: 1,
+//         name: "Rose chep",
+//         job: "Web Developer",
+//         img: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-964894534-1600088334.jpg",
+//         text: "Would recommend anyone to get their glasses here.Top quality!",
+//     },
+//     {
+//         id: 2,
+//         name: "josh Werner",
+//         job:  "Ux Designer",
+//         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeIOfjYdgqb2559aXQSwMOfEfQoCePwANbKw&usqp=CAU",
+//         text: "Since i got this glasses my coding life has improved significantly due to reduced stress on my eyes",
+//     }
+// ];
+// const img = document.getElementById("img");
+//  const author = document.getElementById("author");
+//  const job = document.getElementById("job");
+//  const description = document.getElementById("description");
 
- window.addEventListener('DOMContentLoaded', function() {
-    showPerson(currentItem);
- })
+//  const prev = document.querySelector(".prev");
+//  const next = document.querySelector(".next");
+//  const random = document.querySelector(".random");
 
- function showPerson(person) {
-    const item = reviews[currentItem];
-    img.src = item.img;
-    author.textContent = item.author;
-    job.textContent = item.job;
-    description.textContent = item.description;
- }
+//  let currentItem = 0;
 
- next.addEventListener("click", function() {
-    currentItem++;
+//  window.addEventListener('DOMContentLoaded', function() {
+//     showPerson(currentItem);
+//  })
+
+//  function showPerson(person) {
+//     const item = reviews[currentItem];
+//     img.src = item.img;
+//     author.textContent = item.author;
+//     job.textContent = item.job;
+//     description.textContent = item.description;
+//  }
+
+//  next.addEventListener("click", function() {
+//     currentItem++;
    
-    if(currentItem > reviews.length - 1) {
-        currentItem = 0;
-    }
-    showPerson(currentItem);
- })
- prev.addEventListener("click", function() {
-    currentItem--;
-    if(currentItem = 0) {
-        currentItem < reviews.length - 1;
+//     if(currentItem > reviews.length - 1) {
+//         currentItem = 0;
+//     }
+//     showPerson(currentItem);
+//  })
+//  prev.addEventListener("click", function() {
+//     currentItem--;
+//     if(currentItem = 0) {
+//         currentItem < reviews.length - 1;
         
-    }
-    showPerson(currentItem);
- })
- random.addEventListener("click", function() {
-    currentItem = Math.floor(Math.random() * reviews.length);
-    showPerson();
- })
+//     }
+//     showPerson(currentItem);
+//  })
+//  random.addEventListener("click", function() {
+//     currentItem = Math.floor(Math.random() * reviews.length);
+//     showPerson();
+//  })
+
